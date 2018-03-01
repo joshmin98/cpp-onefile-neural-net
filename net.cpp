85259ba1caae4da5625223ea45684a0183f478b1
Joshua Min <minje@usc.edu>
2018-02-28 23:12:51 -0800

    First commit.

--- net.cpp
+++ net.cpp
@@ -191,18 +191,6 @@ void Net::getResults(std::vector<double> resultVals) const {
 }
 
 int main(int argc, char *argv[]) {
-  std::vector<unsigned int> topology;
-  topology.push_back(3);
-  topology.push_back(2);
-  topology.push_back(1);
-  Net myNet(topology);
-
-  std::vector<double> inputVals;
-  myNet.feedForward(inputVals);
-
-  std::vector<double> targetVals;
-  myNet.backProp(targetVals);
-
-  std::vector<double> resultVals;
-  myNet.getResults(resultVals);
+  std::cout << "Ready to learn." << std::endl;
+  return 0;
 }
